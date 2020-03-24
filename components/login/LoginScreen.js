065519ee06c4b6ae244bf84a7608de0423142aa8
t-okada <t-okada@atware.co.jp>
2020-03-25 08:00:44 +0900

    feature/#4 create login screen style

--- components/login/LoginScreen.js
+++ components/login/LoginScreen.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { View, Text, StyleSheet } from "react-native";
+import { Input, Button } from 'react-native-elements';
 
 
 export class LoginScreen extends React.Component {
     render() {
         return (
             <View style={styles.root}>
-                <Text>ログインだよ</Text>
+                <View style={styles.input}>
+                    <Input placeholder='login id' />
+                </View>
+                <View style={styles.input}>
+                    <Input placeholder='password' />
+                </View>
+                <View style={styles.button}>
+                    <Button title='Login' type='outline' />
+                </View>
             </View>
         )
     }
@@ -18,5 +27,13 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+      padding: 16,
     },
+    input: {
+        marginBottom: 16,
+        width: '100%',
+    },
+    button: {
+        width: '100%',
+    }
   });
