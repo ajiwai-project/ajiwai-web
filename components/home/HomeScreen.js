74535c53ac32cf8374e284191c1a391294d7a18b
t-okada <t-okada@atware.co.jp>
2020-06-16 08:50:46 +0900

    WIP create camera action

--- components/home/HomeScreen.js
+++ components/home/HomeScreen.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { View, StyleSheet, Text } from "react-native";
+import { ImagePicker, Permissions } from 'expo';
+import { Button } from '../utils/Button';
 
 export class HomeScreen extends React.Component {
+    state = {
+        hasCameraRollPermission: null,
+        photo: null,
+    }
+
+    async componentDidMount() {
+        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        this.setState({hasCameraRollPermission: status == 'granted'});
+    }
+
     render() {
+        let { hasCameraRollPermission, photo } = this.state;
+
         return (
             <View style={styles.root}>
+                <Button onPress={this.handleCameraRoll}>写真を撮る</Button>
                 <Text>ホーム画面だよ</Text>
             </View>
         )
+    }
+
+    handleCameraRoll = async () => {
+        let photo = await ImagePicker.launchImageLibraryAsync();
+        this.setState({photo: photo})
     }
 }
 
